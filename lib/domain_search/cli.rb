f2ada7c3a72fc330a87b78053a8665b598261d40
Andrew Capp <acapp909@gmail.com>
2019-11-19 22:22:39 +0000

    updated initial menu options on cli file

--- lib/domain_search/cli.rb
+++ lib/domain_search/cli.rb
@@ -16,7 +16,7 @@ class DomainSearch::CLI
     #binding.pry
     if input == "1" || input == "one"
       puts "Enter domain for search:"
-      search_input = gets.strip
+      search_input = gets.strip.downcase
       search_specific(search_input)
     elsif input == "2" || input == "two"
       puts "Enter keyword for search:"
@@ -25,17 +25,35 @@ class DomainSearch::CLI
     elsif input == "q" || input == "quit"
       puts "Goodbye!"
     else
-      puts "Please enter valid menu option:"
+      puts "Please enter a valid menu option:"
       main_menu
     end
   end
 
-  def second_menu
+  def second_menu(name=nil)
     puts <<-DOC
       Choose number of domain to view:
       N or next to show next 10 options
+      B or back to show previous 10 options
       E or exit to go back to main menu
     DOC
+    #will need way to choose multiple options from array or hash
+    input = gets.strip.downcase
+    #binding.pry
+    if input == "1" || input == "one"
+      search_specific(name)
+    elsif input == "n"|| input == "next"
+      puts "Showing up to next 10 options"
+      second_menu(name)
+    elsif input == "b" || input == "back"
+      puts "Showing previous 10 options"
+      second_menu(name)
+    elsif input == "e" || input == "exit"
+      main_menu
+    else
+      puts "Please enter a valid menu option:"
+      second_menu(name)
+    end
   end
 
   def search_specific(name)
@@ -44,7 +62,14 @@ class DomainSearch::CLI
     Available: Yes
     Price: $50.00
     DOC
+    available = false
+    if available
+      main_menu
+    else
+      puts "Your requested domain is not available.  Here is a list of related domains for sale:"
+      search_keyword(name)
     #Need to add list of similar domains if not available
+    end
   end
 
   def search_keyword(keyword)
@@ -53,7 +78,7 @@ class DomainSearch::CLI
     2. xxxx.xxx
     3. xxxx.xxx
     DOC
-
+    second_menu(keyword)
   end
 
 end
