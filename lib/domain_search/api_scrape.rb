0d64fe9fb152c79cd717baf76bc68717a2cf3aaa
Andrew Capp <acapp909@gmail.com>
2019-11-23 01:02:54 +0000

    move API key and secret to constant

--- lib/domain_search/api_scrape.rb
+++ lib/domain_search/api_scrape.rb
@@ -1,8 +1,11 @@
 class APIScrape
 
+  KEY = "3mM44UaChQSbDX_Cz2Z8oBLkQgCGLqvmQHTAN"
+  SECRET = "Jb9VdYd35vbRNeqVyFygk5"
+
   def self.get_domain(name)
     url = "https://api.ote-godaddy.com/v1/domains/available?domain=#{name}"
-    response = HTTParty.get(url, :headers => {"Authorization" => "sso-key 3mM44UaChQSbDX_Cz2Z8oBLkQgCGLqvmQHTAN:Jb9VdYd35vbRNeqVyFygk5", "accept" => 'application/xml'})
+    response = HTTParty.get(url, :headers => {"Authorization" => "sso-key #{KEY}:#{SECRET}", "accept" => 'application/xml'})
   end
 
   def self.get_domain_list(keyword)
