f8c4907b5920f88de9176542b22b3c2372adecc2
Andrew Capp <acapp909@gmail.com>
2019-11-23 01:08:01 +0000

    add BASEURL constant to APIScrape class

--- lib/domain_search/api_scrape.rb
+++ lib/domain_search/api_scrape.rb
@@ -1,24 +1,25 @@
 class APIScrape
 
+  BASEURL = "https://api.ote-godaddy.com/v1/domains"
   KEY = "3mM44UaChQSbDX_Cz2Z8oBLkQgCGLqvmQHTAN"
   SECRET = "Jb9VdYd35vbRNeqVyFygk5"
 
   def self.get_domain(name)
-    url = "https://api.ote-godaddy.com/v1/domains/available?domain=#{name}"
+    url = "#{BASEURL}/available?domain=#{name}"
     response = HTTParty.get(url, :headers => {"Authorization" => "sso-key #{KEY}:#{SECRET}", "accept" => 'application/xml'})
   end
 
   def self.get_domain_list(keyword)
-    url = "https://api.ote-godaddy.com/v1/domains/suggest?query=#{keyword}&limit=20&waitMS=1000"
-    response = HTTParty.get(url, :headers => {"Authorization" => "sso-key 3mM44UaChQSbDX_Cz2Z8oBLkQgCGLqvmQHTAN:Jb9VdYd35vbRNeqVyFygk5"})
+    url = "#{BASEURL}/suggest?query=#{keyword}&limit=20&waitMS=1000"
+    response = HTTParty.get(url, :headers => {"Authorization" => "sso-key #{KEY}:#{SECRET}"})
   end
 
   def self.get_domain_list_info(domain_list_array)
     domains = []
-    url = "https://api.ote-godaddy.com/v1/domains/available?"
+    url = "#{BASEURL}/available?"
     domain_list_array.each do |object|
       domains << object.name
     end
-    response = HTTParty.post(url, :headers => {"Authorization" => "sso-key 3mM44UaChQSbDX_Cz2Z8oBLkQgCGLqvmQHTAN:Jb9VdYd35vbRNeqVyFygk5", "Content-Type" => "application/json"}, :body => domains.to_json)
+    response = HTTParty.post(url, :headers => {"Authorization" => "sso-key #{KEY}:#{SECRET}", "Content-Type" => "application/json"}, :body => domains.to_json)
   end
 end
